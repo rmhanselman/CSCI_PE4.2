40f0eabd97ea3a7c6e66b5c4da0ead91bb11f21b
rmhanselman <rmhanselman@gmail.com>
2020-09-23 15:45:03 -0700

    Create TicTacToe.cpp
    Added CreateBoard and Main calling CreateBoard methods.

---
+++ TicTacToe.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#incldue <string>
+
+std::vector<std::vecotr<std::string>> CreateBoard{
+  std::vector<std::vector<std::string>> v{{"", "", ""},{"", "", ""},{"", "", ""}}; 
+}
+
+int main(){
+  std::vector<std::vector<std::string>> v = CreateBoard{};
+}
