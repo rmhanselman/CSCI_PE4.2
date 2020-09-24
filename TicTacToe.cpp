87298213d9e5a99e07e9cac590d92e0e7afe6037
Jack <collierjack101@gmail.com>
2020-09-24 14:21:30 -0600

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,8 +1,16 @@
 #include <vector>
 #incldue <string>
 
-std::vector<std::vecotr<std::string>> CreateBoard{
+std::vector<std::vector<std::string>> CreateBoard{
   std::vector<std::vector<std::string>> v{{"", "", ""},{"", "", ""},{"", "", ""}}; 
+}
+
+void displayBoard(std::vector<std::string> input){
+  for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+      std::cout << input[i][j];
+    }
+  }
 }
 
 int main(){
