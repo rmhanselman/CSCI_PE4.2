dc5d4bf70c031a90833708e5773371bdcd0e2d6f
rmhanselman <rmhanselman@gmail.com>
2020-09-24 13:23:12 -0700

    Update TicTacToe.cpp
    Added DisplayBoard function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,8 +1,17 @@
 #include <vector>
 #incldue <string>
 
-std::vector<std::vecotr<std::string>> CreateBoard{
+std::vector<std::vector<std::string>> CreateBoard{
   std::vector<std::vector<std::string>> v{{"", "", ""},{"", "", ""},{"", "", ""}}; 
+}
+
+void DisplayBoard(std::vector<std::vector<std::string>> v){
+  for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+      std::cout << v[i][j];
+    }
+    std::cout << std::endl;
+  }
 }
 
 int main(){
